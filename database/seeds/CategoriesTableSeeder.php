6f8684f0220ae854bc83aa2b436abb149c698ad8
Your Name <you@example.com>
2020-03-18 19:26:51 +0200

    settingPart2&Category

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create([
+            'name'          =>  'Root',
+            //'parent_id'     =>  null,
+        ]);
+
+        factory('App\Models\Category', 10)->create();
+    }
+}
