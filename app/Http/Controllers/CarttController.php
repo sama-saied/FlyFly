4d560b53ccb42899c085b9360d2b2b76da138339
sama <samaabdallah98@gmail.com>
2020-06-03 21:25:26 +0200

    new

---
+++ app/Http/Controllers/CarttController.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cartt;
+use App\Models\Product;
+//use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+
+class CarttController extends Controller
+{
+    public function store(Request $request , $product_id )
+    {
+        $cart=new Cartt;
+        $cart->user_id = Auth::user()->id;
+        $cart->product_id = $product_id;
+       //$cart->first_name = $request->first_name;
+       $cart->first_name =Auth::user()->first_name;
+      // $cart->last_name = $request->last_name;
+       $cart->last_name =Auth::user()->last_name;
+     
+        $cart->product_name = $request->product_name;
+        $cart->product_price = $request->product_price;
+      
+       $cart->product_sale_price = $request->product_sale_price;
+       
+      $cart->quantity= $request->quantity;
+      
+        $cart->save();
+      
+        return redirect()->back();
+    }
+
+
+    public function getCart()
+    {
+        
+        return view('site.pages.CartDisplay');
+    }
+}
